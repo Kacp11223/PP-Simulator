dc079880830a4213b08ccba9c5d8670d9cbe57b9
Kacper Kosek <melkorkosek@gmail.com>
2024-12-13 22:44:25 +0100

    Settery

---
+++ Simulator/Animals.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulator;
+
+internal class Animals
+{
+    private string _description = "Unknown";
+    public string Description
+    {
+        get => _description;
+        init
+        {
+            string shortForm = value.Trim();
+
+            if (shortForm.Length < 3)
+                shortForm = shortForm.PadRight(3, '#');
+
+            if (shortForm.Length > 15)
+            {
+                shortForm = shortForm.Substring(0, 15).TrimEnd();
+
+                if (shortForm.Length < 3)
+                    shortForm = shortForm.PadRight(3, '#');
+            }
+            if (char.IsLower(shortForm[0]))
+                shortForm = char.ToUpper(shortForm[0]) + shortForm.Substring(1);
+
+            _description = shortForm;
+        }
+    }
+    public string Info => $"{Description} <{Size}>";
+
+    public uint Size { get; set; } = 3;
+    
+}
+
--- Simulator/Creature.cs
+++ Simulator/Creature.cs
@@ -1,34 +1,67 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Reflection.Emit;
-using System.Reflection.Metadata.Ecma335;
-using System.Text;
-using System.Threading.Tasks;
+﻿    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Reflection.Emit;
+    using System.Reflection.Metadata.Ecma335;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Xml.Linq;
 
 namespace Simulator;
-
 public class Creature
 {
-    public string Name { get; set; }
-    public int Level { get; set; }
-
-    public Creature(string name, int level)
+    private string _name = "Unknown";
+    private int _level = 1;
+    public Creature(string _name, int _level = 1)
     {
-        Name = name;
-        Level = level;
+        Name = _name;
+        Level = _level;
     }
+    public Creature()
+    {
+    }
+    public string Name
+    {
+        get => _name;
+        init
+        {
+            string shortForm = value.Trim();
 
-    public string Info => $"{Name} [{Level}]";
+            if (shortForm.Length < 3)
+                shortForm = shortForm.PadRight(3, '#');
+
+            if (shortForm.Length > 25)
+            {
+                shortForm = shortForm.Substring(0, 25).TrimEnd();
+
+                if (shortForm.Length < 3)
+                    shortForm = shortForm.PadRight(3, '#');
+            }
+            if (char.IsLower(shortForm[0]))
+
+                shortForm = char.ToUpper(shortForm[0]) + shortForm.Substring(1);
+            _name = shortForm;
+        }
+    }
+    public int Level
+    {
+        get => _level;
+
+        init
+        {
+            if (value < 1) _level = 1;
+            else if (value > 10) _level = 10;
+            else _level = value;
+        }
+    }
+    public void Upgrade()
+    {
+        if (_level < 10)
+            _level++;
+    }
+    
     public void SayHi() =>
-    Console.WriteLine($"Hi, I'm {Name}, my level is {Level}.");
-}
-
-public class Animals
-{
-    public required string Description { get; init; }
-    public uint Size { get; set; } = 3;
-
-    public string Info => $"Dogs: <3>.";
+        Console.WriteLine($"Hi! I'm {Name}, level {Level}.");
+    public string Info => $"Name: {Name}, Level: {Level}";
 }
